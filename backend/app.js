3a2724fbc40a5b03a767feaeb3e269a9cbe11462
Yogendra Rajput <yogendra.leetcode123@gmail.com>
2025-11-24 18:54:16 +0530

    updating

--- backend/app.js
+++ backend/app.js
@@ -1,27 +1,41 @@
 // src/app.js
-const express = require('express');
-const cors = require('cors');
-const authRoutes = require('./routes/auth');      // we'll add these routes next
-const leadsRoutes = require('./routes/leads');
-const employeesRoutes = require('./routes/employees');
-const errorHandler = require('./middlewares/errorHandler');
-const path = require('path');
-
-
-
+const express = require("express");
+const cors = require("cors");
+const authRoutes = require("./routes/auth"); // we'll add these routes next
+const leadsRoutes = require("./routes/leads");
+const employeesRoutes = require("./routes/employees");
+const errorHandler = require("./middlewares/errorHandler");
+const path = require("path");
 
 const app = express();
 
 // middlewares
-app.use('/uploads', express.static(path.join(__dirname, '..', 'uploads')));
+app.use("/uploads", express.static(path.join(__dirname, "..", "uploads")));
 app.use(express.json()); // parse JSON bodies
 app.use(express.urlencoded({ extended: true })); // parse form bodies
-app.use(cors({ origin: "https://plexi-genius.vercel.app" || '*' ,credentials:true})); 
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://plexi-genius.vercel.app",
+];
+
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        console.log("Blocked by CORS:", origin);
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true,
+  })
+);
 
 // routes (placeholders for now - files will be added soon)
-app.use('/api/auth', authRoutes);
-app.use('/api/leads', leadsRoutes);
-app.use('/api/employees', employeesRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/leads", leadsRoutes);
+app.use("/api/employees", employeesRoutes);
 
 // global error handler
 app.use(errorHandler);
